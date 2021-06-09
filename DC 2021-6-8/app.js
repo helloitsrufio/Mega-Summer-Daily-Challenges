f1e31f335b8d6a929cf2f9076a3efb6c58deee28
Ruth Reed <ruthreed.dev@gmail.com>
2021-06-08 22:43:57 -0600

    Add coding challenge for 6-8.

---
+++ DC 2021-6-8/app.js
@@ -0,0 +1,14 @@
+//Find the greatest common divisor of two positive integers. The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+//will always be positive. Will alawys be greater than or equal to 1. Always numbers? Any edge cases I should consider?
+//we want the greatest common divisor of two given integers
+console.log(gcd(6,18),6)
+console.log(gcd(24,18),6)
+console.log(gcd(144,60),12)
+
+function gcd(x,y){
+    //ensure than they are greater than/equal to 1 in a condition
+
+
+}
+//couldn't remember
