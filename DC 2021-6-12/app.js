9c974e167609a04fe6b75bf298bf70ce1363e009
Ruth Reed <ruthreed.dev@gmail.com>
2021-06-12 21:52:21 -0600

    Add coding attempt for challenge on 6-12.

---
+++ DC 2021-6-12/app.js
@@ -0,0 +1,17 @@
+//What is the memo-ize soln for the Fibonacci sequence?
+
+//memoizing basically allows you to store result so no extra work is done on behalf on computer. Ideal way to solve fib sequence problem.
+//Return n!, but memo-ize style!
+console.log(fib(5),120)
+console.log(fib(12),479001600)
+console.log(fib(14),87178291200)
+
+//make a function that takes in a number
+function fib(n){
+    //base case of n>2
+
+    //set memoize var to be empty arr
+
+    //
+}
+//couldn't remember tbh
