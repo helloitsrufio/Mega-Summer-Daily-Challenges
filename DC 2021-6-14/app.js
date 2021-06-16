dfdfbc053a1bf7e6b2508b6643ce368519c318ad
Ruth Reed <ruthreed.dev@gmail.com>
2021-06-15 21:43:44 -0600

    Add soln to coding challenge from 6-14.

--- DC 2021-6-14/app.js
+++ DC 2021-6-14/app.js
@@ -11,3 +11,16 @@ function factorial(n){
     return factorial(n*n-1)
 }
 //too much recursion! It wouldn't work oof
+//Ah we need a base case! that's why it was going fucking crazy
+
+
+//WORK THROUGH SOLN
+//function that takes in a number
+function fac(n){
+    //anything 2 and below will be 1, so if n < 2, return 1
+    //else multiply the num by the function num - 1
+    return n < 2 ? 1 : n * fac(n - 1)
+}
+
+//the defining char of this solution is the base case. a recursive soln NEEDS a base case or else the recursion won't stop.
+//Also, they didn't do factorial(n*n-1), which doesn't work btw, but instead did n * factorial(n-1)
