ec9cb58fbb24fd5d9779de9e0e1aa25b4cc07dcb
Ruth Reed <ruthreed.dev@gmail.com>
2021-06-17 21:27:23 -0600

    Add additional coding solution to challenge from 6-16.

--- DC 2021-6-16/app.js
+++ DC 2021-6-16/app.js
@@ -11,3 +11,22 @@ function updateVegetablesCollection(arr){
 console.log(updateVegetablesCollection(['broccoli', 'sprouts', 'veggie'], 'veggie'), true)
 console.log(updateVegetablesCollection(['broccoli', 'sprouts', 'cucumber'], 'veggie'), false)
 console.log(updateVegetablesCollection(['cauliflower']), false)
+
+
+
+//given a certain veggie
+//given an arr, specific veggie
+function updateVegetablesCollection (arr, veggie) {
+    //if, in the array, the index of specific veggie doesn't exist, push it to array
+    if (arr.indexOf(veggie) === -1) {
+        arr.push(veggie);
+        //otherwise, console log array
+    } else if (arr.indexOf(veggie) > -1) {
+        console.log(arr + ' already exists in the arr collection.');
+    }
+}
+
+console.log(updateVegetablesCollection(['potato', 'tomato', 'chillies', 'green-pepper'], 'spinach'));
+
+console.log(updateVegetablesCollection(['potato', 'tomato', 'chillies', 'green-pepper'], 'chillies'));
+//This isn't working when I try to run it. I'm not sure exactly what it is. I think maybe a refactoring of my own solution might work out better?
