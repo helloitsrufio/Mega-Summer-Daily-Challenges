0a9761de647d2fcb763308b0a224bbad472a3305
Ruth Reed <ruthreed.dev@gmail.com>
2021-06-22 17:16:15 -0600

    Add coding challenge attempt for 6-22-21.

--- DC 2021-6-22/app.js
+++ DC 2021-6-22/app.js
@@ -23,3 +23,12 @@
 // Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 
 //From: https://leetcode.com/problems/fibonacci-number/
+
+//My attempt:
+function fib(num){
+    For(n >  2; n < num.length; n++){
+     let sum = num[n-1] + num[n-2]
+}
+    return num
+}
+//Doesn't work. In retrospect that's partially because I didn't start with an array = [0,1], and then I didn't put the results into arr[num]
