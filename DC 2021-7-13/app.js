f00b7fa3ab2f8452601f8ec7cf9de3e67e2e2fa4
Ruth Reed <ruthreed.dev@gmail.com>
2021-07-13 12:12:45 -0600

    Add coding challenge for 7-13-21.

---
+++ DC 2021-7-13/app.js
@@ -0,0 +1,31 @@
+// Make sure you understand these before taking a look at the question:
+// Replace (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace)
+
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// domainName("http://github.com/carbonfive/raygun") == "github" 
+// domainName("http://www.zombie-bites.com") == "zombie-bites"
+// domainName("https://www.cnet.com") == "cnet
+
+
+
+// From: https://www.codewars.com/kata/514a024011ea4fb54200004b
+
+/*
+Parameters: Can take in special functions. Will alwyas be given a string. Will always be given an address. 
+R: core of the address, aka name of site.
+E: ex. wwww.starfish.com -> starfish
+    ex. wwww.reddit.com -> reddit
+    ex. www.ruthisthebomb.com -> ruthisthebomb
+P: function that takes in one parameter
+    That parameter will be replaced with a new string. 
+    Returned. 
+*/
+
+function coreName(str){
+    return str.replace(x, y)
+}
+
+//Tbh I have no idea how to do this one. I missed the class explanation and can't look up the solns on codewars because it's 5kyu and I'm 7kyu. It's too far above my rank. I feel shitty about not being able to solve it, but also I do take some comfort in its difficulty because of course I am having a hard time solving it! It's 5kyu!
+
+//I hope to return to this when my skill is higher. 
