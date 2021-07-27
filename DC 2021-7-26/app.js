a0ce5ade8e2e7ee04940767e0be8bd32db4e0e39
Ruth Reed <ruthreed.dev@gmail.com>
2021-07-26 18:58:54 -0600

    Add coding challenge for 7-26-21.

---
+++ DC 2021-7-26/app.js
@@ -0,0 +1,21 @@
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+// assert.deepEqual(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
+// assert.deepEqual(sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0]);
+
+// https://www.codewars.com/kata/578aa45ee9fd15ff4600090d
+
+function sortArray(arr){
+    let newArr = arr.filter(el => el % 2 !== 0)
+    newArr = newArr.sort((a,b)=>(a-b))
+    //.sort() is implicitly ascending?
+    return arr.map(el => el % 2 !== 0 ? newArr.shift() : el) 
+  }
+  sortArray([2,3,5,3,14,8,9])
+
+  //Works! In the example, they just used .sort() without anything in the parentheses, and this confused me because in a daily challenge earlier this week, in order to order the #s in ascending order, you had to do (a,b)=>a+b. Someone in my group told me that the empty parentheses work, but only to a certain extent. What I did is preferable, and that made me feel good because I remembered! I remembered (a,b)=>a+b! Active recall/spaced repetition!
