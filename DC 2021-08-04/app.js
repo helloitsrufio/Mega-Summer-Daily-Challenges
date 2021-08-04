424367d6a42d06a1d90a6cdc254389fc5feed402
Ruth Reed <ruthreed.dev@gmail.com>
2021-08-04 16:08:09 -0600

    Add coding challenge for 8-4-21.

---
+++ DC 2021-08-04/app.js
@@ -0,0 +1,14 @@
+//Fibonacci memo-ize soln
+const memo = {}
+var fib = function(n) {
+    if(n <= 1){
+        return n
+    }else if( memo[n]){
+        return memo[n]
+    }else{
+        let result = fib(n-1) + fib(n-2)
+        memo[n] = result
+        return result
+    }
+}
+//Turned from quadratic to linear with each nth number we look @
