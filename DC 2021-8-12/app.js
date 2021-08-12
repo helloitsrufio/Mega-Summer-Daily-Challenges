5138fb78143846d86b015cc7b0a80fba9a8ead63
Ruth Reed <ruthreed.dev@gmail.com>
2021-08-12 17:16:22 -0600

    Add (unsolved) coding question for 8-12-21.

---
+++ DC 2021-8-12/app.js
@@ -0,0 +1,5 @@
+// Given a string of words or phrases, count the number of vowels.
+
+// No Regex! Unless that is your brute force...
+
+//From: https://medium.com/siliconwat/algorithms-in-javascript-b0bed68f4038
