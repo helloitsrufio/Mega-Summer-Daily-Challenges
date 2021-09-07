f8e250285c7992de2ca993c519c87545c290419d
Ruth Reed <ruthreed.dev@gmail.com>
2021-09-07 15:47:13 -0600

    Add description for daily coding challenge.

---
+++ DC 2021-9-7/app.js
@@ -0,0 +1,16 @@
+// You have to create a function calcType, which receives 3 arguments: 2 numbers, and the result of an unknown operation performed on them (also a number).
+
+// Based on those 3 values you have to return a string, that describes which operation was used to get the given result.
+
+// The possible return strings are: "addition", "subtraction", "multiplication", "division".
+// Example:
+
+// calcType(1, 2, 3) -->   1 ? 2 = 3   --> "addition"
+
+// Notes
+
+//     In case of division you should expect that the result of the operation is obtained by using / operator on the input values - no manual data type conversion or rounding should be performed.
+//     Cases with just one possible answers are generated.
+//     Only valid arguments will be passed to the function.
+
+// Only valid arguments will be passed to the function!
