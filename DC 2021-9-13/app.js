65d7422c096e52b629ba7d0d698ea1d98b0c11a1
Ruth Reed <ruthreed.dev@gmail.com>
2021-09-13 15:59:04 -0600

    Add coding challenge for 9-13-21.

---
+++ DC 2021-9-13/app.js
@@ -0,0 +1,18 @@
+// Please read through these before taking a look at the question:
+
+// Map (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map)
+// Reduce (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce)
+// More Array Methods (https://javascript.info/array-methods)
+// Array Methods Complexity (https://dev.to/lukocastillo/time-complexity-big-0-for-javascript-array-methods-and-examples-mlg)
+
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+// getAverage([2,2,2,2]),2)
+// getAverage([1,2,3,4,5,]),3);
+// getAverage([1,1,1,1,1,1,1,2]),1)
+
+//going to use Math.floor()
