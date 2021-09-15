1d20af0f7e926c2034d105c17163ba8320adfbcf
Ruth Reed <ruthreed.dev@gmail.com>
2021-09-14 20:46:55 -0600

    Add class solution to coding challenge.

--- DC 2021-9-13/app.js
+++ DC 2021-9-13/app.js
@@ -16,3 +16,32 @@
 // getAverage([1,1,1,1,1,1,1,2]),1)
 
 //going to use Math.floor()
+
+//Class soln:
+
+//P: Never be empty, will always be all integer vals.
+//R: Returning a rounded-down integer which is the avg
+//E: 
+    function getAverage(arr){
+//P:    
+        let sum = 0
+        for({
+            sum+=arr[i]
+        })
+        return sum/arr.length
+    }
+
+    console.log(getAverage([2,2,2,2]),2)
+    console.log(getAverage([1,2,3,4,5,]),3)
+    console.log(getAverage([1,1,1,1,1,1,1,2]),1)))
+
+    //with arr methods
+    function getAverage(arr){  
+        return Math.floor(arr.reduce((a,c)=> a + c) / arr.length)//div by arr.length because that's the avg/total amount of elements in arr
+            }
+        
+            console.log(getAverage([2,2,2,2]),2)
+            console.log(getAverage([1,2,3,4,5,]),3)
+            console.log(getAverage([1,1,1,1,1,1,1,2]),1)))
+
+            //reduce() time complexity is linear because it's running once for each el of the arr.
