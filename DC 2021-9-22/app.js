da21fce3ceb08ac52390c9705b04965ebb5f9506
Ruth Reed <ruthreed.dev@gmail.com>
2021-09-22 15:52:59 -0600

    Add coding challenge attempts for 9-22-21.

--- DC 2021-9-22/app.js
+++ DC 2021-9-22/app.js
@@ -16,3 +16,50 @@
 
 // From: 
 // https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/javascript
+
+//P:capitalization is ignored. Will it always be a string? Special characters allowed. Is there a min size for string? White spaces counted as chars.
+//R: Return NEW string composed of ( and ) based on frequency.
+//E: See console.logs below
+
+//first attempt:
+
+function duplicateEncode(word){
+    //Make all strings lowercase
+      word.toLowerCase()
+    //map through string and see if index is present.
+      word.map(e => {
+          let index = word.indexOf(e)
+          e > 1 ? e === ) : e === (
+      })
+    
+    //if index is present > 1, replace with )
+    //else replace with (
+    }
+    
+    // Examples
+    // "din"      =>  "((("
+    // "recede"   =>  "()()()"
+    // "Success"  =>  ")())())"
+    // "(( @"     =>  "))((" 
+    
+    console.log(duplicateEncode('din'), '(((')
+    console.log(duplicateEncode('recede'), '()()()')
+    console.log(duplicateEncode('Success'), ')())())')
+    console.log(duplicateEncode('(( @'), '))((')
+    
+    //second attempt
+    function duplicateEncode(word){
+    //Make all strings lowercase
+       word.toLowerCase().split('')
+    //map through string and see if index is present.
+      let e = word[i]
+      let index = word.indexOf(e)
+      word.map(index => {
+          return index > 1 ? e === ')' : index === '('
+      })
+    
+    //if index is present > 1, replace with )
+    //else replace with (
+    }
+
+    //frustrating because I feel like I'm kind of getting there? But then I didn't get solve it, so I need to review a soln.
