4d1855e76f9f86fd9209d24dbe940677ffcbc36d
Ruth Reed <ruthreed.dev@gmail.com>
2021-09-21 22:12:59 -0600

    Add coding challenge for 9-22-21. Unsolved, but added for tomorrow.

---
+++ DC 2021-9-22/app.js
@@ -0,0 +1,18 @@
+//Need to read the following articles before taking a crack at the problem: 
+
+// Map (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map)
+// indexOf(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf)
+// lastIndexOf(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/lastIndexOf)
+
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+
+
+
+// From: 
+// https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/javascript
