7eefe379a2f0c5f6fd1f2623feb644f0de6699fd
Ruth Reed <ruthreed.dev@gmail.com>
2021-09-25 13:50:30 -0600

    Add coding challenge (unsolved) for 9-26-21.

---
+++ DC 2021-9-26/app.js
@@ -0,0 +1,14 @@
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+// last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
+// last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']
+// last('take me to semynak'), ['take', 'me', 'semynak', 'to'] 
+
+
+
+// From: 
+// https://www.codewars.com/kata/57eba158e8ca2c8aba0002a0
