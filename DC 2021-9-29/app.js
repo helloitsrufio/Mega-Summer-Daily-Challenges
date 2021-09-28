27d52c08d5cda5c38fd533c2d70aca65283a1315
Ruth Reed <ruthreed.dev@gmail.com>
2021-09-28 13:35:39 -0600

    Add unsolved coding challenge for 9-29-21.

---
+++ DC 2021-9-29/app.js
@@ -0,0 +1,17 @@
+// Implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+// arrayDiff([], [4,5]), [], "a was [], b was [4,5]"
+
+// arrayDiff([3,4], [3]), [4], "a was [3,4], b was [3]"
+
+// arrayDiff([1,8,2], []), [1,8,2], "a was [1,8,2], b was []"
+
+
+// From: https://www.codewars.com/kata/523f5d21c841566fde000009
