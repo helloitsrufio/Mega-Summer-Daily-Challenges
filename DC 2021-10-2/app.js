e750324fcf71662018e573800e7b4f9735baa4ee
Ruth Reed <ruthreed.dev@gmail.com>
2021-10-01 13:37:32 -0600

    Add coding challenge for 10-2-21.

---
+++ DC 2021-10-2/app.js
@@ -0,0 +1,25 @@
+// You need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers.
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+
+
+
+
+
+
+
+
+// Hint: spread array, sort it and grab the middle value. Throw that into indexOf the array itself
+
+// From: https://www.codewars.com/kata/545a4c5a61aa4c6916000755
