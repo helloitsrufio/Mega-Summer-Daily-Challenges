8cd8577a772de08348b8354745626cb178b8aeac
Ruth Reed <ruthreed.dev@gmail.com>
2021-10-03 18:01:03 -0600

    Add codewars and teacher solution for 10-2-21 coding challenge.

--- DC 2021-10-2/app.js
+++ DC 2021-10-2/app.js
@@ -63,3 +63,43 @@ function gimme(arr){
     }
 }
 //Ok, with this attempt, I am getting -1 again. So at least it isn't defined, but I really am not any closer than I was before with the simpler answer. 
+
+//__________________________________
+
+//Teacher soln
+function gimme(arr){
+    return arr.indexOf([...arr].sort((a,b)=>a-b[1]))
+}
+//Let's think through this...
+//So we are sorting in descending order with [1] (middle el I believe). The sort is attached to a spread arr. And all of this is arr.indexOf. So the stuff in arr.indexOf() is all just getting that middle element.
+//Tbh I don't really understand this. 
+
+//Then my teacher has, in addition, this:
+var gimme = function (inputArray) {
+    //if the first el is less than the second, and is greater than the third, OR the first el is greater than the second and less than the third, return the index of that first el.
+    if((inputArray[0] < inputArray[1] && inputArray[0] > inputArray[2]) || (inputArray[0] > inputArray[1] && inputArray[0] < inputArray[2])){
+        return 0;
+    }
+
+    //if the second el is less than the first and greater than the third, OR the second el is greater than the first and less than the third, return the index of the second el.
+    if((inputArray[1] < inputArray[0] && inputArray[1] > inputArray[2]) || (inputArray[1] > inputArray[0] && inputArray[1] <inputArray[2])){
+        return 1;
+    }
+
+    //if the third el is less than the first and greater than the second, OR it is greater than the first and less than the second, return the index of the third el. 
+    if((inputArray[2] < inputArray[0] && inputArray[2] > inputArray[1]) || (inputArray[2] > inputArray[0] && inputArray[2] < inputArray[1])){
+        return 2;
+    }
+    //This second soln makes slightly more sense, but also is extremely long and cumbersome. I don't understand setting the whole thing equal to the val of 'gimme', of why gimme being a function with all this happening enables it to use the index nums without having a for loop for i. 
+}
+
+//Another soln from codewars:
+//Makes the whole thing ascending order
+const ascending = (a,b) => a - b
+//assigning the val of gimme to a function that takes in arr
+const gimme = arr => 
+//getting the index of
+//Calling .slice() with no args copies the entire array.
+//So you sort this copied array in ascending order, and then pull out the middle var. 
+arr.indexOf(arr.slice().sort(ascending)[1])
+//I understand what's going on here, but I don't understand how this is going to pull out any indices besides [1].
