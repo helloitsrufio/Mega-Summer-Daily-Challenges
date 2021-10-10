dd11246679184e8b8403f2b5f6480f7d9ccdef19
Ruth Reed <ruthreed.dev@gmail.com>
2021-10-10 15:20:47 -0600

    Add coding challenge for 10-11-21.

---
+++ DC 2021-10-11/app.js
@@ -0,0 +1,5 @@
+// Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+// --> "alpha beta gamma delta alpha beta gamma delta" 
