fbb0f40b0be77e75bafea62a480723b31bc3c520
Ruth Reed <ruthreed.dev@gmail.com>
2021-11-12 15:50:15 -0700

    Add unsolved coding challenge for 11-12-21.

---
+++ DC 2021-11-12/app.js
@@ -0,0 +1,14 @@
+// Your job is to figure out the index of which vowel is missing from a given string:
+
+// A has an index of 0,
+// E has an index of 1,
+// I has an index of 2,
+// O has an index of 3,
+// U has an index of 4.
+// Notes: There is no need for string validation and every sentence given will contain all vowles but one. Also, you won't need to worry about capitals.
+
+// Examples
+// "John Doe hs seven red pples under his bsket"          =>  0  ; missing: "a"
+// "Bb Smith sent us six neatly arranged range bicycles"  =>  3  ; missing: "o"
+
+// From: https://www.codewars.com/kata/56414fdc6488ee99db00002c
