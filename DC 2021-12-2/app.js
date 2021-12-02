973d1afe43107c4aecd99752a6259324095a5855
Ruth Reed <ruthreed.dev@gmail.com>
2021-12-02 16:15:43 -0700

    Add unsolved coding challenge for 12/2/21.

---
+++ DC 2021-12-2/app.js
@@ -0,0 +1,16 @@
+// Given an array nums of size n, return the majority element.
+//arr of nums of any size
+//majority means that you'll have one num that has more instances than any other one num.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+// From: https://leetcode.com/problems/majority-element/
