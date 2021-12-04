3a936a7487ff2a3d0c3bce0fe7b38368d3e6166e
Ruth Reed <ruthreed.dev@gmail.com>
2021-12-04 15:20:43 -0700

    Add unsolved coding challenge for 12-5-21.

---
+++ DC 2021-12-5/app.js
@@ -0,0 +1,22 @@
+// Given a magazine of words and a ransom note, determine if it’s possible to “cut out” and create the ransom note from the magazine words.
+
+// const magazine =
+//  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+
+// describe("Ransom Note", () => {
+//  it("Should return true", () => {
+//   assert.equal(ransomNote("sit ad est sint", magazine), true);
+//  });
+
+// it("Should return false", () => {
+//   assert.equal(ransomNote("sit ad est love", magazine), false);
+//  });
+
+// it("Should return true", () => {
+//   assert.equal(ransomNote("sit ad est sint in in", magazine), true);
+//  });
+
+// it("Should return false", () => {
+//   assert.equal(ransomNote("sit ad est sint in in in in", magazine), false);
+//  });
+// });
