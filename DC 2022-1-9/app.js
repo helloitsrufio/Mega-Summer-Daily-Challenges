b36456f5e1fd2042d35ff0632b2045cab8bc6e30
Ruth Reed <ruthreed.dev@gmail.com>
2022-01-10 20:03:55 -0700

    Add alternative solution to coding challenge from 1-8.

--- DC 2022-1-9/app.js
+++ DC 2022-1-9/app.js
@@ -12,3 +12,24 @@ function occurrencesOfA(arr){
 }
 
 //OMG FIRST FUCKING TRYYYYYYYYY. I'm so pumped! I just worked through it and then got the answer!
+
+//so this is the other soln I had and honestly I kinda hate it. But let's explain it:
+//function taking in an array
+function occurrencesOfA(array){
+    //making an empty arr
+    var indices = [];
+    //setting element to be equal to 'a'
+    var element = 'a';
+    //setting a var idx to get the index of 'a'
+    var idx = array.indexOf(element);
+    //white the index of 'a' exists,
+    while (idx != -1) {
+        //push it to the indices arr
+      indices.push(idx);
+      //then set the index of a equal to the index of 'a' and index of 'a' plus one.
+      idx = array.indexOf(element, idx + 1);
+    }
+    
+    console.log(indices);
+    // [0, 2, 4]
+    }
